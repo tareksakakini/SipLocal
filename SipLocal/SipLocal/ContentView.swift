dfe694c3c08c9a5147be68a42ab5aec5489cfcf2
Tarek Sakakini <tarek.sakakini@gmail.com>
2025-07-07 08:49:33 -0700

    Add SipLocal as a normal directory

---
+++ SipLocal/SipLocal/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SipLocal
+//
+//  Created by Tarek Sakakini on 7/7/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SipLocal/SipLocal/SipLocalApp.swift
@@ -0,0 +1,17 @@
+//
+//  SipLocalApp.swift
+//  SipLocal
+//
+//  Created by Tarek Sakakini on 7/7/25.
+//
+
+import SwiftUI
+
+@main
+struct SipLocalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
